2498b54c52297501cc3dc0d8b19f25475fe7812e
Michael <mobrien@colgate.edu>
2020-01-18 16:52:43 -0500

    updated with imports

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,22 @@
-const fs = require("fs");
-
-fs.writeFileSync("notes.txt", "This file was created by Node.js!");
-
+// const fs = require("fs");
+// fs.writeFileSync("notes.txt", "This file was created by Node.js!");
 // note: first arg is the name of the file, second, is the contents. if file doesn't exist,
 // then it will be created. if file exists, it'll be overwritten
 
-fs.appendFileSync("notes.txt", "\nHi I am the appended info!!!!");
+// fs.appendFileSync("notes.txt", "\nHi I am the appended info!!!!");
+
+// const utils =
+// const add = require("./utils.js");
+
+// const name = "Michael";
+
+// console.log(add);
+
+// const sum = add(1, 2);
+
+// console.log(sum);
+
+const getNotes = require("./notes.js");
+const msg = getNotes();
+
+console.log(msg);
