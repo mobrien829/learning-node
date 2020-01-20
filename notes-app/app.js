d2f08a95b6ddc492a6b77b8eeb40a5c7c03fc11e
Michael <mobrien@colgate.edu>
2020-01-19 23:32:31 -0500

    reviewed arrow functions within objects

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,53 +1,63 @@
-// const fs = require("fs");
-// fs.writeFileSync("notes.txt", "This file was created by Node.js!");
-// note: first arg is the name of the file, second, is the contents. if file doesn't exist,
-// then it will be created. if file exists, it'll be overwritten
-
-// fs.appendFileSync("notes.txt", "\nHi I am the appended info!!!!");
-
-// ********************************
-
-// const utils =
-// const add = require("./utils.js");
-
-// const name = "Michael";
-
-// console.log(add);
-
-// const sum = add(1, 2);
-
-// console.log(sum);
-
-// ****************************
 const chalk = require("chalk");
-// import chalk from "chalk";
-const validator = require("validator");
-// import validator from "validator";
 const yargs = require("yargs");
-const getNotes = require("./notes.js");
-const msg = getNotes();
+const notes = require("./notes.js");
 
-// console.log(msg);
-// console.log(validator.isEmail("mobrien@lol.edu"));
-// console.log(validator.isURL("twitch.tv"));
-
-// *****************************
-
-// console.log(chalk.bgRgb(15, 200, 50).bold.underline.inverse("Success!!"));
-
-// *******************************
-
-const command = process.argv[2];
-
-// if (command === "add") {
-//   console.log(chalk.green("adding note"));
-// } else if (command === "remove") {
-//   console.log(chalk.red("removing note"));
-// }
-
-// below line customizes yargs version if you dont want 1.0.0
 yargs.version("1.1.0");
 
-// Create add command with yargs
+// Add
 
-console.log(yargs.argv);
+yargs.command({
+  command: "add",
+  describe: "adds a new note",
+  //   builders are objects that hold options for a command
+  builder: {
+    title: {
+      describe: "title of note",
+      demandOption: true,
+      type: "string"
+      //   demandOption is like :requires in Rails, it determines if option is required for valid input
+    },
+    body: {
+      describe: "contents of note",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.addNote(argv.title, argv.body)
+});
+
+// Remove
+
+yargs.command({
+  command: "remove",
+  describe: "removes a note",
+  builder: {
+    title: {
+      describe: "title of note",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => notes.removeNote(argv.title)
+});
+
+// Read
+
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: () => console.log(chalk.blue("Reading the note"))
+});
+
+// List
+
+yargs.command({
+  command: "list",
+  describe: "lists all notes",
+  handler: () => console.log(chalk.rgb(10, 250, 200)("Listing all notes"))
+});
+
+yargs.parse();
+// prints it out without doubling the way console.log(yargs.argv) does
+
+// console.log(yargs.argv);
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,3 +1,55 @@
+const fs = require("fs");
+const chalk = require("chalk");
 const getNotes = () => "Your notes...";
 
-module.exports = getNotes;
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    //   returns empty array if no data
+    return [];
+  }
+};
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log(chalk.green("new note added"));
+  } else {
+    console.log(chalk.red("note title taken"));
+  }
+
+  console.log(notes);
+};
+
+const removeNote = title => {
+  const notes = loadNotes();
+  const newArray = notes.filter(note => !(note.title === title));
+
+  if (newArray.length === notes.length) {
+    console.log(chalk.bgKeyword("red")("couldn't find note with that title"));
+  } else {
+    saveNotes(newArray);
+    console.log(chalk.bgKeyword("green")("note removed"));
+  }
+};
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote
+};
